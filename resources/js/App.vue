ca081d95f4c2a218d97ac772b9f118188e2f4b6a
Makonegit <vilaikoneslot@gmail.com>
2024-01-14 16:13:37 +0700

    first commit makonemini

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Vue App11</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MakoneminiAPP',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
